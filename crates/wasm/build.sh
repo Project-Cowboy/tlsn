204a3c152035e8b0e8164da5df1dbc980da6d90c
sinu <65924192+sinui0@users.noreply.github.com>
2025-03-03 11:13:02 -0800

    bench alpha7

--- crates/wasm/build.sh
+++ crates/wasm/build.sh
@@ -1,3 +1,13 @@
-RUSTFLAGS='-C target-feature=+atomics,+bulk-memory,+mutable-globals' \
-    rustup run nightly \
-    wasm-pack build --target web . -- -Zbuild-std=panic_abort,std
+#!/bin/sh
+
+# This crate must be built using the nightly Rust compiler with specific flags.
+# This script automates the build process.
+
+set -e
+
+# Clean up older builds
+rm -rf pkg
+
+# Build tlsn_wasm package
+wasm-pack build --target web .
+
